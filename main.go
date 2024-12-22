166596afb172e081beb613cb2c6c6d96c9df69c0
vedaRadev <ryancarmstrong97@gmail.com>
2024-12-21 22:36:01 -0500

    serve image

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
     serveMux := http.NewServeMux()
-    serveMux.Handle("/", http.FileServer(http.Dir("res")))
+    serveMux.Handle("/", http.FileServer(http.Dir("site")))
 
     server := http.Server { Handler: serveMux, Addr: ":8080" }
 
