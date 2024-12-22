18ef66bc91c0d3cf0b41adfd053b509f949e8095
vedaRadev <ryancarmstrong97@gmail.com>
2024-12-21 22:24:08 -0500

    serve simple index.html

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 
 func main() {
     serveMux := http.NewServeMux()
+    serveMux.Handle("/", http.FileServer(http.Dir("res")))
+
     server := http.Server { Handler: serveMux, Addr: ":8080" }
+
     server.ListenAndServe()
 }
