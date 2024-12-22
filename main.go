9ba410642d6355b0102f1886b0a7f60d9bad9e0d
vedaRadev <ryancarmstrong97@gmail.com>
2024-12-21 22:56:08 -0500

    readiness endpoint

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 
 func main() {
     serveMux := http.NewServeMux()
-    serveMux.Handle("/", http.FileServer(http.Dir("site")))
+    serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("site"))))
+    serveMux.HandleFunc("/healthz", func(res http.ResponseWriter, req *http.Request) {
+        res.WriteHeader(200)
+        res.Header().Add("Content-Type", "text/plain; charset=utf-8")
+        res.Write([]byte("OK"))
+    })
 
     server := http.Server { Handler: serveMux, Addr: ":8080" }
-
     server.ListenAndServe()
 }
